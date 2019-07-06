fa8eba6d11587f23ff9b679596f50d8218b0b791
pereudin <a.a.pereudin@gmail.com>
2019-07-06 09:40:22 +0300

    Initial commit

---
+++ src/server/Server.java
@@ -0,0 +1,60 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.Vector;
+
+public class Server {
+    Vector <ClientHandler> clients;
+
+    public Server() throws SQLException {
+        AuthService.connect();
+//        System.out.println(AuthService.getNickByLoginAndPass("login1","pass1"));
+
+        ServerSocket server = null;
+        Socket socket = null;
+
+        try {
+            clients = new Vector<>();
+            server = new ServerSocket(8189);
+            System.out.println("Сервер запущен");
+
+            while(true){
+                socket = server.accept();
+                System.out.println("Клиент подключился");
+                new ClientHandler(this,socket);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            AuthService.disconnect();
+        }
+    }
+
+    public void broadcastMsg(String str){
+        for (ClientHandler o: clients) {
+            o.sendMsg(str);
+        }
+    }
+
+    public void subscribe(ClientHandler clientHandler){
+        clients.add(clientHandler);
+    }
+
+    public void unsubscribe(ClientHandler clientHandler){
+        clients.remove(clientHandler);
+    }
+}
